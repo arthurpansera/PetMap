fb8ade684657000e265f2b8d59cd0463c4b3fcc5
arthurpansera <163167633+arthurpansera@users.noreply.github.com>
2025-03-28 10:07:00 -0300

    Banco de Dados - PetMap
    Atualização do Banco de Dados

--- petmap-db.sql
+++ petmap-db.sql
@@ -4,10 +4,15 @@ USE petmap;
 CREATE TABLE usuario (
     id_usuario INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    telefone VARCHAR(20),
     senha VARCHAR(255) NOT NULL,
     PRIMARY KEY (id_usuario)
+);
+
+CREATE TABLE contato (
+    id_usuario INT NOT NULL,
+    telefone VARCHAR(20),
+    email VARCHAR(100) NOT NULL UNIQUE,
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
 );
 
 CREATE TABLE moderador (
@@ -16,6 +21,29 @@ CREATE TABLE moderador (
     PRIMARY KEY (id_moderador),
     FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario) ON DELETE CASCADE
 );
+
+CREATE TABLE ong {
+    cnpj VARCHAR(18) NOT NULL UNIQUE,
+    id_usuario INT NOT NULL,
+    endereco_rua VARCHAR(100) NOT NULL,
+    endereco_numero VARCHAR(10) NOT NULL,
+    endereco_complemento VARCHAR(50),
+    endereco_bairro VARCHAR(50) NOT NULL,
+    endereco_cidade VARCHAR(50) NOT NULL,
+    endereco_estado CHAR(2) NOT NULL,
+    endereco_pais VARCHAR(20) NOT NULL,
+    endereco_cep CHAR(8) NOT NULL,
+    PRIMARY KEY (cnpj),
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario) ON DELETE CASCADE
+}
+
+CREATE TABLE cidadao {
+    cpf VARCHAR(14) NOT NULL UNIQUE,
+    id_usuario INT NOT NULL,
+    data_nasc DATE NOT NULL,
+    PRIMARY KEY (cpf),
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario) ON DELETE CASCADE
+}
 
 CREATE TABLE perfil (
     id_perfil INT NOT NULL AUTO_INCREMENT,
